df091b28368828b366788c3ded434ec227224949
manu <mechin.emmanuel@gmail.com>
2018-01-18 23:31:36 +0100

    random color is more precise on high luminosity points

--- artCode/src/apps/tools.js
+++ artCode/src/apps/tools.js
@@ -26,5 +26,43 @@ function getRandomColor(luminosity) {
     }
     if (r1 > 1/2) {r1 = 1/2}
     if (r2-r1 > 1/2) {r2 = r1+1/2}
-    return [parseInt(luminosity*r1), parseInt(luminosity*(r2-r1)), parseInt(luminosity*(1-r2))]
+
+    var l1, l2, l3;
+    if (luminosity < 255*3/2) {
+        l1 = parseInt(luminosity*r1);
+        l2 = parseInt(luminosity*(r2-r1));
+        l3 = parseInt(luminosity*(1-r2));
+    }
+    else {
+        l1 = 255-parseInt((255*3-luminosity)*r1);
+        l2 = 255-parseInt((255*3-luminosity)*(r2-r1));
+        l3 = 255-parseInt((255*3-luminosity)*(1-r2));
+    }
+    var r;
+    for (var i=0; i<10; i++) {
+        if (l1 > 255 || l2 > 255 || l3 > 255) {
+            if (l1 > 255) {
+                r = Math.random();
+                l1 = 255;
+                l2 += r*(l1-255);
+                l3 += (1-r)*(l1-255);
+            }
+            if (l2 > 255) {
+                r = Math.random();
+                l1 += r*(l2-255);
+                l2 = 255;
+                l3 += (1-r)*(l2-255);
+            }
+            if (l3 > 255) {
+                r = Math.random();
+                l1 += r*(l3-255);
+                l2 += (1-r)*(l3-255);
+                l3 = 255;
+            }
+        }
+        else {
+            break
+        }
+    }
+    return [parseInt(l1), parseInt(l2), parseInt(l3)]
 }
