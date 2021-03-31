ac8b0979ed00c4c07300da36421b4e7814e3deb9
Emmanuel Méchin <mechin.emmanuel@lilo.org>
2021-03-31 19:32:09 +0200

    clean link component

--- app/components/Link/index.js
+++ app/components/Link/index.js
@@ -8,12 +8,10 @@ import { Link as RouterLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-export const Link = styled(RouterLink).attrs(({ fontSize }) => ({
-  fontSize,
-}))`
+export const Link = styled(RouterLink)`
   color: inherit;
   text-decoration: none;
-  font-size: ${props => props.fontSize}px;
+  font-size: ${props => props.fontSize || 10}px;
 `;
 
 Link.propTypes = { fontSize: PropTypes.number };
